e8f9f1e59bbfff6c31d63818e339356c96504e2c
Satgoy152 <satyamgoyal@gmail.com>
2024-08-02 23:48:59 -0700

    Improved parsing systems

--- audit_parse.py
+++ audit_parse.py
@@ -14,14 +14,25 @@ def extract_text_fromaudit(uploaded_file)->str:
         for page in pages:
             audit_text.append(page.extract_text())
 
-
+    # check if valid degree audit
+    if len(audit_text) < 2:
+        return "Invalid PDF"
+    
+    if audit_text[0].find("Degree Audit") == -1:
+        return "Invalid PDF"
     # remove header from pages 1 to end
     for i in range(1, len(audit_text)):
         start = audit_text[i].find("- In Progress")
-        audit_text[i] = audit_text[i][start + 13:]
+        if start != -1:
+            audit_text[i] = audit_text[i][start + 13:]
+        else:
+            start = audit_text[i].find(" In Progress")
+            if start != -1:
+                audit_text[i] = audit_text[i][start + 15:]
         # remove last page after Course History
     end = audit_text[-1].find("Course History")
-    audit_text[-1] = audit_text[-1][:end]
+    if end != -1:
+        audit_text[-1] = audit_text[-1][:end]
 
         # concatenate all pages
     concat_text = ""
