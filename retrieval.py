6bd62ba892e8ef44f14800ac9699f6908e6d7ccb
Satgoy152 <satyamgoyal@gmail.com>
2024-08-16 23:52:51 -0700

    retriver

--- retrieval.py
+++ retrieval.py
@@ -31,6 +31,6 @@ class Retriever:
         saved_data_store = Chroma(persist_directory="./chroma_db", collection_name="umich_fa2024", embedding_function=embeddings, client=new_client)
         saved_data_store_dummy = Chroma(persist_directory="./chroma_db", collection_name="umich_fa2024", embedding_function=dummyEmbeddings, client=new_client)
 
-        self.retriver_sim = saved_data_store.as_retriever(search_type="similarity_score_threshold", search_kwargs={"k": 10, "score_threshold": 0.5})
+        self.retriver_sim = saved_data_store.as_retriever(search_type="similarity_score_threshold", search_kwargs={"k": 10, "score_threshold": 0.6})
         self.retriever_dummy = saved_data_store_dummy.as_retriever(search_type="similarity_score_threshold", search_kwargs={"k": 1, "score_threshold": 0.99})
 
